4dbe5cb2b3d54e739e147d3b5bc33340181622df
kien3010 <145182889+kien3010@users.noreply.github.com>
2023-10-03 16:08:26 +0700

    thêm phần lựa chọn tần số cắt của các bộ lọc

--- Buoi3_Loctinhieu.py
+++ Buoi3_Loctinhieu.py
@@ -1,107 +1,173 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from scipy.io import wavfile
-import tkinter as tk
-from tkinter import ttk
 from sympy import symbols, lambdify
 from sympy import sin, pi
 
-# Import data
-Fs, signal = wavfile.read('Music2.mp3')
-signal = signal[:, 1]  # Right channel
-signalLen = len(signal)
+def get_input_values():
+    fs_input = entry_fs.get()
+    T_input = entry_T.get()
+    ff1_input = entry_ff1.get()
+    ff2_input = entry_ff2.get()
+    ff3_input = entry_ff3.get()
 
-plt.figure(figsize=(10, 10))
+    try:
+        Fs = float(fs_input)
+        T = float(T_input)
+        ff1 = float(ff1_input)
+        ff2 = float(ff2_input)
+        ff3 = float(ff3_input)
+    except ValueError:
+        messagebox.showerror("Lỗi", "Vui lòng nhập số hợp lệ")
+        return None
 
-# Original Signal
-plt.subplot(5, 1, 1)
-stepLen = 1
-range = np.arange(0, signalLen, stepLen)
-plt.plot(range, signal[range])
-plt.title('Original Signal')
-plt.ylabel('Amp')
+    return Fs, T, ff1, ff2, ff3
 
-# Initialization
-mu = 0.1
-noise = np.random.randn(len(signal))  # Gaussian noise
-noise1 = signal.filtfilt([1, 0.5], 1, noise)  # Noise 1
-noise2 = signal.filtfilt([1, -0.5], 1, noise)  # Noise 2
-sn = signal + noise1  # Signal + Noise
+def apply_filter():
+    input_values = get_input_values()
+    if input_values is None:
+        return
 
-plt.subplot(5, 1, 2)
-plt.plot(range, sn[range])
-plt.title('Signal + Noise')
-plt.ylabel('Amp')
+    Fs, T, ff1, ff2, ff3 = input_values
 
-# Filtering
-numWeights = 32
-LMS_filter = signal.dlti([1], [1, -0.5], dt=1)  # Discrete LTI Filter
-_, s_output, _ = signal.dlsim(LMS_filter, noise2, x0=None)
-s_output = s_output[:, 0]
+    t = np.linspace(0, T, int(T * Fs), endpoint=False)
+    t_symbolic = symbols('t')
+    signal_with_noise = (
+            sin(2 * pi * ff1 * t_symbolic) +
+            sin(2 * pi * ff2 * t_symbolic) +
+            sin(2 * pi * ff3 * t_symbolic)
+    )
+    signal_with_noise_fn = lambdify(t_symbolic, signal_with_noise, modules='numpy')
+    signal_with_noise_values = signal_with_noise_fn(t)
 
-# Results
-plt.subplot(5, 1, 3)
-plt.plot(range, s_output[range])
-plt.title('Filtered Signal')
-plt.ylabel('Amp')
+    filter_type = combo_filter.get()
 
-plt.subplot(5, 1, 4)
-plt.plot(range, noise2[range])
-plt.title('Filtering Error')
-plt.ylabel('Amp')
+    if filter_type == "Thông cao":
+        cutoff_frequency = float(entry_cutoff.get())  # Lấy tần số cắt từ trường nhập liệu
+        nyquist_frequency = 0.5 * Fs
+        normal_cutoff = cutoff_frequency / nyquist_frequency
+        b, a = signal.butter(4, normal_cutoff, btype='high', analog=False)
+        filtered_signal = signal.filtfilt(b, a, signal_with_noise_values)
+    elif filter_type == "Thông dải":
+        low_cutoff = float(entry_low_cutoff.get())  # Lấy tần số cắt thấp từ trường nhập liệu
+        high_cutoff = float(entry_high_cutoff.get())  # Lấy tần số cắt cao từ trường nhập liệu
+        nyquist_frequency = 0.5 * Fs
+        normal_low_cutoff = low_cutoff / nyquist_frequency
+        normal_high_cutoff = high_cutoff / nyquist_frequency
+        b, a = signal.butter(4, [normal_low_cutoff, normal_high_cutoff], btype='band', analog=False)
+        filtered_signal = signal.filtfilt(b, a, signal_with_noise_values)
+    elif filter_type == "Dải chắn":
+        low_cutoff = float(entry_low_cutoff.get())  # Lấy tần số cắt thấp từ trường nhập liệu
+        high_cutoff = float(entry_high_cutoff.get())  # Lấy tần số cắt cao từ trường nhập liệu
+        nyquist_frequency = 0.5 * Fs
+        normal_low_cutoff = low_cutoff / nyquist_frequency
+        normal_high_cutoff = high_cutoff / nyquist_frequency
+        b, a = signal.butter(4, [normal_low_cutoff, normal_high_cutoff], btype='bandstop', analog=False)
+        filtered_signal = signal.filtfilt(b, a, signal_with_noise_values)
+    elif filter_type == "Thông thấp":
+        cutoff_frequency = float(entry_cutoff.get())  # Lấy tần số cắt từ trường nhập liệu
+        nyquist_frequency = 0.5 * Fs
+        normal_cutoff = cutoff_frequency / nyquist_frequency
+        b, a = signal.butter(4, normal_cutoff, btype='low', analog=False)
+        filtered_signal = signal.filtfilt(b, a, signal_with_noise_values)
+    else:
+        messagebox.showerror("Lỗi", "Bộ lọc không hợp lệ")
+        return
 
-plt.subplot(5, 1, 5)
-plt.plot(range, np.abs(s_output[range] - signal[range]))
-plt.title('System Error')
-plt.xlabel('Time index')
-plt.ylabel('Amp')
+    plt.figure(figsize=(10, 6))
+    plt.subplot(2, 1, 1)
+    plt.plot(t, signal_with_noise_values, 'b-', label='Tín hiệu gốc')
+    plt.xlabel('Thời gian (s)')
+    plt.ylabel('Amplitude')
+    plt.legend()
 
-plt.figure()
-plt.stem(LMS_filter.B, use_line_collection=True)
-plt.title('Filter Weights')
-plt.xlabel('ith weight')
-plt.ylabel('Value')
-plt.grid(True)
+    plt.subplot(2, 1, 2)
+    plt.plot(t, filtered_signal, 'r-', label='Tín hiệu sau lọc')
+    plt.xlabel('Thời gian (s)')
+    plt.ylabel('Amplitude')
+    plt.legend()
 
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+def filter_type_changed(event):
+    selected_filter = combo_filter.get()
+    if selected_filter in ["Thông thấp", "Thông cao"]:
+        label_cutoff.grid(row=7, column=0)
+        entry_cutoff.grid(row=7, column=1)
+        label_low_cutoff.grid_remove()
+        entry_low_cutoff.grid_remove()
+        label_high_cutoff.grid_remove()
+        entry_high_cutoff.grid_remove()
+    elif selected_filter in ["Thông dải", "Dải chắn"]:
+        label_cutoff.grid_remove()
+        entry_cutoff.grid_remove()
+        label_low_cutoff.grid(row=7, column=0)
+        entry_low_cutoff.grid(row=7, column=1)
+        label_high_cutoff.grid(row=8, column=0)
+        entry_high_cutoff.grid(row=8, column=1)
+
 # Tạo cửa sổ giao diện
 window = tk.Tk()
 window.title("Nhập thông số")
 
 # Tạo các thành phần giao diện
 label_fs = tk.Label(window, text="Tần số lấy mẫu (Fs):")
-label_fs.pack()
+label_fs.grid(row=0, column=0)
 entry_fs = tk.Entry(window)
-entry_fs.pack()
+entry_fs.grid(row=0, column=1)
 
 label_T = tk.Label(window, text="Thời gian thu thập (T):")
-label_T.pack()
+label_T.grid(row=1, column=0)
 entry_T = tk.Entry(window)
-entry_T.pack()
+entry_T.grid(row=1, column=1)
 
 label_ff1 = tk.Label(window, text="Tần số ff1:")
-label_ff1.pack()
+label_ff1.grid(row=2, column=0)
 entry_ff1 = tk.Entry(window)
-entry_ff1.pack()
+entry_ff1.grid(row=2, column=1)
 
 label_ff2 = tk.Label(window, text="Tần số ff2:")
-label_ff2.pack()
+label_ff2.grid(row=3, column=0)
 entry_ff2 = tk.Entry(window)
-entry_ff2.pack()
+entry_ff2.grid(row=3, column=1)
 
 label_ff3 = tk.Label(window, text="Tần số ff3:")
-label_ff3.pack()
+label_ff3.grid(row=4, column=0)
 entry_ff3 = tk.Entry(window)
-entry_ff3.pack()
+entry_ff3.grid(row=4, column=1)
 
 label_filter = tk.Label(window, text="Chọn bộ lọc:")
-label_filter.pack()
+label_filter.grid(row=5, column=0)
 combo_filter = ttk.Combobox(window, values=["Thông cao", "Thông dải", "Dải chắn", "Thông thấp"])
-combo_filter.pack()
+combo_filter.grid(row=5, column=1)
+
+# Thêm trường nhập liệu cho tần số cắt
+label_cutoff = tk.Label(window, text="Tần số cắt:")
+entry_cutoff = tk.Entry(window)
+
+# Thêm trường nhập liệu cho tần số cắt thấp và tần số cắt cao
+label_low_cutoff = tk.Label(window, text="Tần số cắt thấp:")
+entry_low_cutoff = tk.Entry(window)
+label_high_cutoff = tk.Label(window, text="Tần số cắt cao:")
+entry_high_cutoff = tk.Entry(window)
+
+# Gán sự kiện thay đổi loại bộ lọc
+combo_filter.bind("<<ComboboxSelected>>", filter_type_changed)
 
 button_apply = tk.Button(window, text="Áp dụng bộ lọc", command=apply_filter)
-button_apply.pack()
+button_apply.grid(row=9, columnspan=2)
+
+# Mặc định ẩn trường nhập liệu tần số cắt
+label_cutoff.grid_remove()
+entry_cutoff.grid_remove()
+label_low_cutoff.grid_remove()
+entry_low_cutoff.grid_remove()
+label_high_cutoff.grid_remove()
+entry_high_cutoff.grid_remove()
 
 # Hiển thị cửa sổ
 window.mainloop()
