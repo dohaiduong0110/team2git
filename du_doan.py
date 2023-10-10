0ded2fb2c751b9ab04ea85b4add62d38ab10ec2f
Trinh Bao Kien <pikachukien2812@gmail.com>
2023-10-10 16:08:50 +0700

    thêm biểu đồ

--- du_doan.py
+++ du_doan.py
@@ -1,95 +1,50 @@
-import os
+import tensorflow as tf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow.compat.v1 as tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-tf.disable_v2_behavior()
-# Generating random linear data
-# There will be 50 data points ranging from 0 to 50
-'''df=pd.read_csv('Student_Performance.csv',index_col=0,header = 0)
-x = array(df.iloc[:900,:5])
-y = array(df.iloc[:900,5:6])
-'''
-#Tạo tâp giá trị x và y
-x = np.linspace(0, 50, 50)
-y = np.linspace(0, 50, 50)
 
-# Cộng thêm nhiễu cho tập x và y để có tập dữ liệu ngẫu nhiên
-x += np.random.uniform(-4, 4, 50)
-y += np.random.uniform(-4, 4, 50)
-n = len(x) # Số lượng dữ liệu
+# Đọc dữ liệu từ tệp Excel
+data = pd.read_csv('Student_Performance.csv')
 
-# Plot of Training Data
-plt.scatter(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title("Training Data")
-plt.show()
-# Tạo model cho tập dữ liệu
-X = tf.placeholder("float")
-Y = tf.placeholder("float")
-# khởi tạo biến w và b
-W = tf.Variable(np.random.randn(), name = "W")
-b = tf.Variable(np.random.randn(), name = "b")
-# thiết lập tốc độ học
-learning_rate = 0.01
-# số vòng lặp
-training_epochs = 100
-# Hàm tuyến tính
-y_pred = tf.add(tf.multiply(X, W), b)
+# Chia dữ liệu thành các đặc trưng và nhãn
+X = data.drop('Hours Studied', axis=1)
+y = data['Performance Index']
 
-# Mean Squared Error Cost Function
-cost = tf.reduce_sum(tf.pow(y_pred-Y, 2)) / (2 * n)
+# Chuẩn hóa dữ liệu
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
-# Tối ưu bằng Gradient Descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Thiết lập Global Variables
-init = tf.global_variables_initializer()
-# Starting the Tensorflow Session
-with tf.Session() as sess:
+# Xây dựng mô hình
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(1)
+])
 
-    # Initializing the Variables
-    sess.run(init)
+# Biên dịch mô hình với hàm mất mát MSE và trình tối ưu
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-    # Iterating through all the epochs
-    for epoch in range(training_epochs):
+# Huấn luyện mô hình
+history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))
 
-        # Feeding each data point into the optimizer using Feed Dictionary
-        for (_x, _y) in zip(x, y):
-            sess.run(optimizer, feed_dict = {X : _x, Y : _y})
+# Đánh giá mô hình trên tập kiểm tra
+loss = model.evaluate(X_test, y_test)
+print(f'Mean Squared Error on Test Data: {loss}')
 
-        # Displaying the result after every 50 epochs
-        if (epoch + 1) % 50 == 0:
-            # Calculating the cost a every epoch
-            c = sess.run(cost, feed_dict = {X : x, Y : y})
-            print("Epoch", (epoch + 1), ": cost =", c, "W =", sess.run(W), "b =", sess.run(b))
+# Lấy giá trị mất mát trong quá trình huấn luyện
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-    # Storing necessary values to be used outside the Session
-    training_cost = sess.run(cost, feed_dict ={X: x, Y: y})
-    weight = sess.run(W)
-    bias = sess.run(b)
-# Calculating the predictions
-predictions = weight * x + bias
-print("Training cost =", training_cost, "Weight =", weight, "bias =", bias, '\n')
-# Plotting the Results
-plt.plot(x, y, 'ro', label ='Original data')
-plt.plot(x, predictions, label ='Fitted line')
-plt.title('Linear Regression Result')
+# Vẽ biểu đồ mất mát
+epochs = range(1, len(train_loss) + 1)
+plt.plot(epochs, train_loss, 'b', label='Training Loss')
+plt.plot(epochs, val_loss, 'r', label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
 plt.legend()
 plt.show()
-import pandas as pd
 
-# Đọc dữ liệu từ file CSV
-df = pd.read_csv('Student_Performance.csv')
-
-# Lấy cột Hours Studied và Performance Index
-hours = df['Hours Studied']
-performance = df['Performance Index']
-
-# Plot biểu đồ scatter để xem mối quan hệ
-plt.scatter(hours, performance)
-plt.xlabel('Hours Studied')
-plt.ylabel('Performance Index')
-plt.title('Relationship between Hours Studied and Performance')
-plt.show()
