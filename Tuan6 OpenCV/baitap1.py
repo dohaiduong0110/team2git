e1c5a2ffb9b2357261b5bb6d40ebcf3b460c66e7
Trinh Bao Kien <pikachukien2812@gmail.com>
2023-10-17 14:55:44 +0700

    thay đổi zoom ảnh, giao diện

--- Tuan6 OpenCV/baitap1.py
+++ Tuan6 OpenCV/baitap1.py
@@ -1,58 +1,92 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-def zoom_image(x, y):
-    img = cv2.imread('meo.jpg', cv2.IMREAD_GRAYSCALE)
-    zoomed = cv2.resize(img, (x, y))
-    Titles = ["Original", "Zoomed"]
-    images = [img, zoomed]
 
-    for i in range(2):
-        plt.subplot(2, 1, i+1)
-        plt.title(Titles[i])
-        plt.imshow(images[i])
-    plt.show()
+# Hàm để cập nhật và hiển thị ảnh sau khi zoom và xoay
+def update_image():
+    if original_img is not None:
+        # Áp dụng zoom và xoay vào ảnh gốc
+        zoomed_img = original_img.resize((int(original_img.width * current_scale), int(original_img.height * current_scale)), Image.LANCZOS)
+        rotated_img = zoomed_img.rotate(rotation_angle)
 
-def get_values():
-    x = int(x_entry.get())
-    y = int(y_entry.get())
-    zoom_image(x, y)
+        img = ImageTk.PhotoImage(rotated_img)
+        img_label.config(image=img)
+        img_label.image = img
 
-def load_image():
+        # Cập nhật tỷ lệ zoom
+        zoom_label.config(text=f"Zoom: {current_scale:.1f}")
+
+
+# Hàm xử lý sự kiện khi thanh trượt thay đổi giá trị
+def on_scale_change(event):
+    global current_scale
+    current_scale = scale.get()
+    update_image()
+
+
+# Hàm xử lý sự kiện khi nút "Chọn ảnh" được nhấn
+def select_image():
+    global original_img, current_scale
     file_path = filedialog.askopenfilename()
     if file_path:
-        img = Image.open(file_path)
-        img = ImageTk.PhotoImage(img)
-        canvas.create_image(0, 0, anchor="nw", image=img)
-        canvas.image = img
+        original_img = Image.open(file_path)
+        current_scale = 1.0
+        rotation_angle = 0  # Đặt góc xoay về 0 độ
+        update_image()
 
-root = tk.Tk()
-root.title("Image Zoom App")
 
-frame = tk.Frame(root)
-frame.pack()
+# Hàm xử lý sự kiện khi nút "Xoay trái" được nhấn
+def rotate_left():
+    global rotation_angle
+    rotation_angle -= 90
+    update_image()
 
-x_label = tk.Label(frame, text="Enter x:")
-x_label.pack()
-x_entry = tk.Entry(frame)
-x_entry.pack()
 
-y_label = tk.Label(frame, text="Enter y:")
-y_label.pack()
-y_entry = tk.Entry(frame)
-y_entry.pack()
+# Hàm xử lý sự kiện khi nút "Xoay phải" được nhấn
+def rotate_right():
+    global rotation_angle
+    rotation_angle += 90
+    update_image()
 
-zoom_button = tk.Button(frame, text="Zoom Image", command=get_values)
-zoom_button.pack()
 
-load_button = tk.Button(frame, text="Load Image", command=load_image)
-load_button.pack()
+# Tạo cửa sổ
+window = tk.Tk()
+window.title("Zoom and Rotate Image")
+window.geometry('950x450')
 
-canvas = tk.Canvas(root, width=500, height=500)
-canvas.pack()
+# Khai báo biến toàn cục
+current_scale = 1.0
+original_img = None
+rotation_angle = 0
 
-root.mainloop()
+# Tạo frame chứa thanh trượt và nút chọn ảnh
+control_frame = tk.Frame(window)
+control_frame.pack(side="left")
+
+# Tạo thanh trượt
+scale = tk.Scale(control_frame, from_=0.1, to=2.0, resolution=0.1, orient=tk.HORIZONTAL, command=on_scale_change)
+scale.set(current_scale)
+scale.pack()
+
+# Tạo nút "Chọn ảnh" và đặt sự kiện khi nút này được nhấn
+select_button = tk.Button(control_frame, text="Chọn ảnh", command=select_image)
+select_button.pack()
+
+# Tạo nút "Xoay trái" và đặt sự kiện khi nút này được nhấn
+rotate_left_button = tk.Button(control_frame, text="Xoay trái", command=rotate_left)
+rotate_left_button.pack()
+
+# Tạo nút "Xoay phải" và đặt sự kiện khi nút này được nhấn
+rotate_right_button = tk.Button(control_frame, text="Xoay phải", command=rotate_right)
+rotate_right_button.pack()
+
+# Label hiển thị ảnh
+img_label = tk.Label(window)
+img_label.pack()
+
+# Label hiển thị tỷ lệ zoom
+zoom_label = tk.Label(control_frame, text=f"Zoom: {current_scale:.1f}")
+zoom_label.pack()
+
+window.mainloop()
