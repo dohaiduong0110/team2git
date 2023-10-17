64f70aff8e6f2a092ab8766fc2123619344b4f95
Phan Văn Kiên <145182889+kien3010@users.noreply.github.com>
2023-10-17 13:56:06 +0700

    Thêm giao diện và có thể chọn ảnh từ máy tính

--- Tuan6 OpenCV/baitap1.py
+++ Tuan6 OpenCV/baitap1.py
@@ -1,18 +1,58 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv2.imread('BannerHaUI.jpg', cv2.IMREAD_GRAYSCALE)
-def resize_image(image, scale_x, scale_y):
-    return cv2.resize(image, (0, 0), fx=scale_x, fy=scale_y)
-scale_x = float(input("Nhập tỉ lệ x (zoom in > 1, zoom out < 1): "))
-scale_y = float(input("Nhập tỉ lệ y (zoom in > 1, zoom out < 1): "))
-zoomed_image = resize_image(img, scale_x, scale_y)
-plt.figure(figsize=(10, 5))
-plt.figure(1)
-plt.title("Anh goc")
-plt.imshow(img, cmap='gray')
-plt.figure(2)
-plt.title(" Anh zoom")
-plt.imshow(zoomed_image, cmap='gray')
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
-plt.show()
+def zoom_image(x, y):
+    img = cv2.imread('meo.jpg', cv2.IMREAD_GRAYSCALE)
+    zoomed = cv2.resize(img, (x, y))
+    Titles = ["Original", "Zoomed"]
+    images = [img, zoomed]
+
+    for i in range(2):
+        plt.subplot(2, 1, i+1)
+        plt.title(Titles[i])
+        plt.imshow(images[i])
+    plt.show()
+
+def get_values():
+    x = int(x_entry.get())
+    y = int(y_entry.get())
+    zoom_image(x, y)
+
+def load_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        img = Image.open(file_path)
+        img = ImageTk.PhotoImage(img)
+        canvas.create_image(0, 0, anchor="nw", image=img)
+        canvas.image = img
+
+root = tk.Tk()
+root.title("Image Zoom App")
+
+frame = tk.Frame(root)
+frame.pack()
+
+x_label = tk.Label(frame, text="Enter x:")
+x_label.pack()
+x_entry = tk.Entry(frame)
+x_entry.pack()
+
+y_label = tk.Label(frame, text="Enter y:")
+y_label.pack()
+y_entry = tk.Entry(frame)
+y_entry.pack()
+
+zoom_button = tk.Button(frame, text="Zoom Image", command=get_values)
+zoom_button.pack()
+
+load_button = tk.Button(frame, text="Load Image", command=load_image)
+load_button.pack()
+
+canvas = tk.Canvas(root, width=500, height=500)
+canvas.pack()
+
+root.mainloop()
