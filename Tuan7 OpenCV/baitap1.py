9fcb6a0fef7acca0a36b0c3267333d59600fa543
dohaiduong0110 <111473155+dohaiduong0110@users.noreply.github.com>
2023-10-24 14:52:00 +0700

    Update baitap1.py
    Update final version

--- Tuan7 OpenCV/baitap1.py
+++ Tuan7 OpenCV/baitap1.py
@@ -1,15 +1,40 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-img=cv2.imread('BannerHaUI.jpg',cv2.IMREAD_GRAYSCALE)
-half=cv2.resize(img, (0,0),fx=0.1,fy=0.1)
-bigger=cv2.resize(img,(1050,1610))
-stretch_near=cv2.resize(img, (780,540),interpolation=cv2.INTER_LINEAR)
-Titles=["Original", "Half", "Bigger","Interpolation Nearest"]
-images=[img,half,bigger,stretch_near]
-count=4
-for i in range(count):
-  plt.subplot(2,2,i+1)
-  plt.title(Titles[i])
-  plt.imshow(images[i])
-plt.show()
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+def open_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        image = cv2.imread(file_path)
+        return image
+    return None
+
+def blur_image():
+    image = open_image()
+    if image is not None:
+        x, y, width, height = 100, 270, 150, 150
+        roi = image[y:y+height, x:x+width]
+        ksize = (21, 21)
+        sigma = 0
+        blurred_roi = cv2.GaussianBlur(roi, ksize, sigma)
+        image[y:y+height, x:x+width] = blurred_roi
+        cv2.imwrite('blurred_image.jpg', image)
+        messagebox.showinfo("Success", "Image blurred and saved as 'blurred_image.jpg'")
+        cv2.imshow('Original Image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# Tạo cửa sổ giao diện
+root = tk.Tk()
+root.title("Image Blurring")
+
+# Tạo nút để mở bức ảnh
+open_button = tk.Button(root, text="Open Image", command=open_image)
+open_button.pack()
+
+# Tạo nút để làm mờ bức ảnh
+blur_button = tk.Button(root, text="Blur Image", command=blur_image)
+blur_button.pack()
+
+root.mainloop()
